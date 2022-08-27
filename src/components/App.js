4d85fbb787a167cefd307d049ce0afdfc0bfbae5
Praveen Bankhede <100555115+praveen1295@users.noreply.github.com>
2022-08-27 07:51:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para,setPara]=useState(false);
   return (
     <div id="main">
         {para ? (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>) :null} 
